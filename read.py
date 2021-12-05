39f48efafbb2c6f8cf51155d656f0565916ffc6e
aj63236 <59002970+aj63236@users.noreply.github.com>
2021-12-05 12:02:36 +0800

    Version 2

--- read.py
+++ read.py
@@ -5,8 +5,20 @@ with open('reviews.txt' , 'r') as file_one:
 	for line in file_one:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 10000 == 0:
 			print(len(data)/10000,'%')
 
 
-print(len(data))
+print("檔案讀取完成,總共有",len(data),"筆資料")
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print("全部留言的平均長度是", sum_len/len(data))
+
+#[len(data[1]),.....,len(data[1000000])]
+#for num in data:
+	#data_two.append(len(num))
+	#print(data_two[0])
+
